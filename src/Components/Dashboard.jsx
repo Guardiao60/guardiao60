d2b871264d7f0c8bc3da0d7e4f8c90af97ddbc7f
Karen Carvalho <86808543+karencarvalho04@users.noreply.github.com>
2026-08-20 13:10:43 +0000

    adição do dashboard

---
+++ src/Components/Dashboard.jsx
@@ -0,0 +1,13 @@
+
+
+function Dashboard() {
+  return (
+    <section id="contato">
+      <h2>Dados</h2>
+      <div>
+        <iframe title="Guardiao60+_Dash" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiZGVlZmRlNmQtMjE1My00MGQxLTlkZDAtYjNhOWUwN2E4OTIwIiwidCI6IjFlNGFiZTE1LTRjNzgtNDBiYy04YmViLTA4OWNlOThhMjJjYyJ9%22" frameborder="0" allowFullScreen="true"></iframe>
+      </div>
+    </section>
+  );
+}
+export default Dashboard;
--- src/Pages/Dados/index.js
+++ src/Pages/Dados/index.js
@@ -1,7 +1,10 @@
+import Dashboard from "../../Components/Dashboard";
+
 function Dados(){   
     return(
         <div>
             <h1>Dados</h1>
+            <Dashboard/>
        </div>
     );
 }
