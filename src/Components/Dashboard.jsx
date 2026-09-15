a3d912ede76eaa48ae42f1f5bc56bd271009ff0e
HeloisaRaizer <heloisaraizer0@gmail.com>
2026-09-15 10:31:54 -0300

    Update Dashboard component structure

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -3,10 +3,10 @@
 function Dashboard() {
   return (
     <section id="contato">
-      <h2>Dados</h2>
+
       <div>
-    <h1>Dashboard Guardião 60+</h1>
- 
+    
+ 	
     <iframe
         title="Guardiao60+_Dash"
         width="100%"
