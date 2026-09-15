a46ccf4574c1d3196109c70e865d4de6ad896f0b
HeloisaRaizer <heloisaraizer0@gmail.com>
2026-09-15 10:29:18 -0300

    Update index.js

--- src/Pages/Dados/index.js
+++ src/Pages/Dados/index.js
@@ -1,12 +1,22 @@
+import React, { useState } from "react";
+import "../../App.css";
+import Header from "../../Components/Header";
 import Dashboard from "../../Components/Dashboard";
+import Footer from "../../Components/Footer";
 
-function Dados(){   
-    return(
-        <div>
-            <h1>Dados</h1>
-            <Dashboard/>
-       </div>
-    );
+function Dados() {
+  
+  return (
+    <main className="home">
+      <Header/>
+      <Dashboard/>
+      <Footer/>
+    </main>
+     );
 }
 
+    
 export default Dados;
+
+
+
