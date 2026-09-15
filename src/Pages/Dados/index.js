d05a891216011407f27226859f36694b8b972382
Karen Carvalho <86808543+karencarvalho04@users.noreply.github.com>
2026-09-15 10:47:04 -0300

    Remove useState import from index.js

--- src/Pages/Dados/index.js
+++ src/Pages/Dados/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "../../App.css";
 import Header from "../../Components/Header";
 import Dashboard from "../../Components/Dashboard";
